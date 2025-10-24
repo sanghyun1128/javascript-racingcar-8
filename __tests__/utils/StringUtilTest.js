13c483550c1271f02152639be8fff772bb4410d2
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-24 14:03:13 +0900

    test(StringUtil): 기본 구분자로 문자열 분할 기능 테스트 추가

---
+++ __tests__/utils/StringUtilTest.js
@@ -0,0 +1,21 @@
+import StringUtil from '../../src/utils/StringUtil.js';
+import DEFAULT_VALUES from '../../src/consts/default_values.js';
+
+const D = DEFAULT_VALUES.DEFAULT_DELIMITER;
+
+describe('splitByDefaultDelimiter 테스트', () => {
+  test.each([
+    [`aa a${D}sss${D}ddd`, ['aa a', 'sss', 'ddd']],
+    [`aaa${D}${D}bbb`, ['aaa', '', 'bbb']],
+    [`aaa${D}`, ['aaa', '']],
+  ])('기본 동작', (input, output) => {
+    expect(() => StringUtil.splitByDefaultDelimiter(input)).toEqual(output);
+  });
+
+  test.each([
+    [` a aa   ${D}  sss ${D} ddd`, ['a aa', 'sss', 'ddd']],
+    [`aaa${D}  ${D}bbb`, ['aaa', '', 'bbb']],
+  ])('개별 항목 앞뒤 공백 처리', (input, output) => {
+    expect(() => StringUtil.splitByDefaultDelimiter(input)).toEqual(output);
+  });
+});
