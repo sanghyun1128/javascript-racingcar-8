4803d14131802dca6d4ca79716fc788294ce6f88
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-26 02:02:12 +0900

    feat(App): 메인 로직 구현
    ApplicationTest 통과

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import IOManager from './io/IOManager.js';
+import RaceManager from './domain/RaceManager.js';
+import StringUtil from './utils/StringUtil.js';
+import Validator from './utils/Validator.js';
+
 class App {
-  static async run() {
+  async run() {
     try {
-    } catch (error) {}
+      // 1. 입력 받기
+      IOManager.printRequestNames();
+      const names = await IOManager.getString();
+      IOManager.printRequestNumOfRounds();
+      const numOfRounds = await IOManager.getNumber();
+
+      // 2. 입력 가공
+      const nameArray = StringUtil.splitByDefaultDelimiter(names);
+      Validator.validateNoDuplicates(nameArray);
+      Validator.validateNumOfParticipants(nameArray.length);
+
+      // 3. 레이스 진행
+      const raceManager = new RaceManager(numOfRounds);
+      nameArray.forEach((e) => raceManager.addParticipant(e));
+      raceManager.startRace();
+
+      // 4. 결과 표시
+      IOManager.printEmptyLine();
+      IOManager.printResultHeader();
+      raceManager.getRoundResults().forEach((e) => {
+        IOManager.printRoundResult(e);
+        IOManager.printEmptyLine();
+      });
+      IOManager.printWinner(raceManager.retrieveWinners());
+    } catch (error) {
+      error.message = IOManager.applyErrorMessageFormat(error.message);
+      throw error;
+    }
   }
 }
 
