41f51585818432fd31fc2798fe083f81048bbdc7
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-26 00:58:08 +0900

    feat(IOManager): 입출력 함수들 구현
    IOManagerTest 통과

--- src/io/IOManager.js
+++ src/io/IOManager.js
@@ -1,17 +1,57 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+import Validator from '../utils/Validator.js';
+import MESSAGES from '../consts/messages.js';
+import DEFAULT_VALUES from '../consts/default_values.js';
+
 class IOManager {
-  static async getString() {}
+  static async getString() {
+    const string = await MissionUtils.Console.readLineAsync();
+    Validator.validateString(string);
 
-  static async getNumber() {}
+    return string.trim();
+  }
 
-  static printRequestNames() {}
+  static async getNumber() {
+    const number = await MissionUtils.Console.readLineAsync();
+    Validator.validateNumber(number);
 
-  static printRequestNumOfRounds() {}
+    return Number(number.trim());
+  }
 
-  static printWinner(nameArray) {}
+  static printRequestNames() {
+    MissionUtils.Console.print(MESSAGES.REQUEST_NAMES);
+  }
 
-  static printRoundResult(roundResult) {}
+  static printRequestNumOfRounds() {
+    MissionUtils.Console.print(MESSAGES.REQUEST_NUM_OF_ROUNDS);
+  }
 
-  static setErrorMessage(error) {}
+  static printEmptyLine() {
+    MissionUtils.Console.print('\n');
+  }
+
+  static printResultHeader() {
+    MissionUtils.Console.print(MESSAGES.RESULT_HEADER);
+  }
+
+  static printWinner(nameArray) {
+    MissionUtils.Console.print(
+      `${MESSAGES.WINNER_PREFIX}${MESSAGES.SEPARATOR}${nameArray.join(`${DEFAULT_VALUES.DEFAULT_DELIMITER} `)}`,
+    );
+  }
+
+  static printRoundResult(roundResult) {
+    roundResult.forEach((e) => {
+      MissionUtils.Console.print(
+        `${e.name}${MESSAGES.SEPARATOR}${MESSAGES.PROGRESS_INDICATOR.repeat(e.position)}}`,
+      );
+    });
+  }
+
+  static applyErrorMessageFormat(errorMessage) {
+    return `${MESSAGES.ERROR_PREFIX}${MESSAGES.SEPARATOR}${errorMessage}`;
+  }
 }
 
 export default IOManager;
