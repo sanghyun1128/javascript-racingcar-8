b37422b4de3e3eeca6c616ee928fd1336dd2cb09
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-25 22:38:01 +0900

    feat(StringUtil): splitByDefaultDelimiter 함수 구현
    StringUtilTest 통과

--- src/utils/StringUtil.js
+++ src/utils/StringUtil.js
@@ -1,5 +1,12 @@
+import DEFAULT_VALUES from '../consts/default_values.js';
+
 class StringUtil {
-  static splitByDefaultDelimiter(string) {}
+  static splitByDefaultDelimiter(string) {
+    const items = string.split(DEFAULT_VALUES.DEFAULT_DELIMITER);
+    const trimmedItems = items.map((e) => e.trim());
+
+    return trimmedItems;
+  }
 }
 
 export default StringUtil;
