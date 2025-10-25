ad2bc55569c4ea869d0316ece517d97b3a8396a7
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-25 22:17:32 +0900

    feat(Validator): 검증에 사용할 로직 구현
    ValidatorTest 통과

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,13 +1,43 @@
+import ERROR_MESSAGES from '../consts/error_messages.js';
+import LIMIT_VALUES from '../consts/limit_values.js';
+
 class Validator {
-  static validateString(string) {}
+  static validateString(string) {
+    const trimmedString = string.trim();
+    const stringLength = trimmedString.length;
 
-  static validateNumber(number) {}
+    if (stringLength === 0) throw new Error(ERROR_MESSAGES.EMPTY_INPUT);
+    if (stringLength > LIMIT_VALUES.MAX_INPUT_LENGTH) throw new Error(ERROR_MESSAGES.LONG_INPUT);
+  }
 
-  static validateName(name) {}
+  static validateNumber(number) {
+    const trimmedNumber = number.trim();
 
-  static validateNumOfParticipants(participants) {}
+    if (Number.isNaN(+trimmedNumber)) throw new Error(ERROR_MESSAGES.NOT_NUMBER);
+    if (+trimmedNumber >= LIMIT_VALUES.MAX_SAFE_INTEGER) throw new Error(ERROR_MESSAGES.BIG_NUMBER);
+  }
 
-  static validateNumOfRounds(numOfRound) {}
+  static validateName(name) {
+    const trimmedName = name.trim();
+
+    if (trimmedName.length > LIMIT_VALUES.MAX_NAME_LENGTH)
+      throw new Error(ERROR_MESSAGES.LONG_NAME);
+
+    const invalidCharRegex = /[^A-Za-z0-9\uAC00-\uD7A3\s]/;
+    if (invalidCharRegex.test(trimmedName)) throw new Error(ERROR_MESSAGES.NOT_ALLOWED_CHARACTER);
+  }
+
+  static validateNumOfParticipants(numOfParticipants) {
+    if (numOfParticipants > LIMIT_VALUES.MAX_PARTICIPANTS)
+      throw new Error(ERROR_MESSAGES.MAX_PARTICIPANTS);
+    if (numOfParticipants < LIMIT_VALUES.MIN_PARTICIPANTS)
+      throw new Error(ERROR_MESSAGES.MIN_PARTICIPANTS);
+  }
+
+  static validateNumOfRounds(numOfRounds) {
+    if (numOfRounds > LIMIT_VALUES.MAX_ROUNDS) throw new Error(ERROR_MESSAGES.MAX_ROUNDS);
+    if (numOfRounds < LIMIT_VALUES.MIN_ROUNDS) throw new Error(ERROR_MESSAGES.MIN_ROUNDS);
+  }
 }
 
 export default Validator;
